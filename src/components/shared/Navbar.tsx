e49858f3c0a0adeb9c39acd8697170f3d861fb74
elene638 <elenejavaxishvili@yahoo.com>
2023-02-15 05:16:43 -0800

    Add navbar component

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -3,6 +3,8 @@ import Logo from "../../assets/images/download.png"
 import "./navbar.css"
 import { useAppSelector, useAppDispatch } from '../../store'
 import { sidebar } from '../../store/NavbarSlice'
+import { categories } from "../../assets/data/categories"
+import { Link, useMatch } from 'react-router-dom'
 
 const Navbar: React.FC = () => {
 
@@ -18,6 +20,16 @@ const Navbar: React.FC = () => {
                 <img className='navbar-logo' src={Logo} alt="logo" />
             </div>
             <h1 className='navbar-title'>Lost and Found</h1>
+
+            <div className="category-list-navbar">
+                {/* <Link to="/" className='category-list-item active-link'><div>HOME</div></Link> */}
+                {categories.map(category => {
+                    const match = useMatch(category.to)
+                    return (
+                        <Link to={category.to} className={`${match ? "category-list-item-navbar active-link-navbar" : "category-list-item-navbar"}`}><div>{category.title}</div></Link>
+                    )
+                })}
+            </div>
         </div>
     )
 }
