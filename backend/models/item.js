c072713413222a6cd983234b7c9494338d072942
elene638 <elenejavaxishvili@yahoo.com>
2023-03-14 01:28:03 -0700

    Create item schema

---
+++ backend/models/item.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  lost_date: { type: String, required: true },
+  location: { type: String, required: true },
+  image: { type: String, required: true },
+  other: { type: String },
+  address: {
+    longitude: { type: Number, required: true },
+    latitude: { type: Number, required: true },
+  },
+  user: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Item", itemSchema);
